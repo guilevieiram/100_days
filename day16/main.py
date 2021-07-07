073045b814858998f464947b23db5b00e77c33de
guilherme-vieira <guilhermevmanhaes@gmail.com>
2021-07-07 16:14:19 -0300

    added files for coffee machine project

---
+++ day16/main.py
@@ -0,0 +1,15 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+'''
+Goals:
+1. Print report.
+2. Check resources sufficient?
+3. Process coins
+4. Check transaction successful?
+5. Make Coffee.
+
+'''
+
+class CoffeeMachine():
