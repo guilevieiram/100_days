7bde4d205d274ab7166f82f57b1bcd2fcbac66df
Guilherme Vieira <guilhermevmanhaes@gmail.com>
2021-07-07 18:45:58 -0300

    made coffee machine

--- day16/main.py
+++ day16/main.py
@@ -1,6 +1,7 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+import os
 
 '''
 Goals:
@@ -13,3 +14,88 @@ Goals:
 '''
 
 class CoffeeMachine():
+	def __init__(self) -> None:
+		self.menu: Menu = Menu()
+		self.money_machine: MoneyMachine = MoneyMachine()
+		self.coffee_maker: CoffeeMaker = CoffeeMaker()
+		self.money_recieved_total: float = 0
+
+	def print_report(self) -> None:
+		'''Prints a full report of the coffee machine'''
+		print(f"\nMoney report:")
+		self.money_machine.report()
+		print(f"\nCoffee Maker report:")
+		self.coffee_maker.report()
+
+	def check_sufficient_resources(self, drink_name: str) -> bool:
+		'''Check if the machine has suffiecient resources for a given order from the menu'''
+		drink: MenuItem = self.menu.find_drink(drink_name)
+		return self.coffee_maker.is_resource_sufficient(drink)
+
+	def process_coins(self, drink_name: str) -> None:
+		'''Processess coins from a given order'''
+		drink: MenuItem = self.menu.find_drink(drink_name)
+		print(f"Drink cost: {drink.cost}")
+
+	def check_transaction_successful(self) -> bool:
+		'''checks if a transaction was successful'''
+		cost: float = self.money_machine.money_received
+		return self.money_machine.make_payment(cost)
+
+	def make_coffee(self, drink_name: str) -> None:
+		'''Takes an order and makes the coffee'''
+		drink: MenuItem = self.menu.find_drink(drink_name) 
+		self.coffee_maker.make_coffee(drink)
+
+
+	def is_drink_name_valid(self, drink_name: str) -> bool:
+		return isinstance(self.menu.find_drink(drink_name), MenuItem)
+
+
+
+
+def main() -> None:
+	coffee: CoffeeMachine = CoffeeMachine()
+
+	on = True
+
+	while on:
+
+		os.system("clear")
+		print("-----------------------------------------------")
+
+		'''Getting the drink order'''
+		print("What would you like to drink today?\nHere are the options!")
+		print(coffee.menu.get_items())
+		drink_name: str = input("Drink: ")
+
+		if coffee.is_drink_name_valid(drink_name):
+
+			'''Checking resourses'''
+			if coffee.check_sufficient_resources(drink_name):
+
+				coffee.process_coins(drink_name)
+
+				if coffee.check_transaction_successful():
+					coffee.make_coffee(drink_name)
+
+				else: print("Transaction unsuccessful")
+
+			else: print("Not suficcient resourses")
+
+
+		else:
+			print(f"{drink_name} is not a valid option ...")
+
+
+		power: str = input("Turn off machine (y/n)? ")	
+		if power.lower() == "y": on = False
+
+	
+	os.system("clear")	
+	print("REPORT:")
+	coffee.print_report()
+
+
+if __name__=='__main__':
+	main()
