2c054a31b6911ae060dd800671fc126e2b10b06b
guilherme-vieira <guilhermevmanhaes@gmail.com>
2021-07-18 21:14:27 -0300

    fixed bugs

--- french_flash/model.py
+++ french_flash/model.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 """CONSTANTS"""
 # Data
-CSV_PATH = 'data/french_words.csv'
+COMPLETE_CSV_PATH = 'data/french_words.csv'
 TO_LEARN_CSV_PATH = 'data/words_to_learn.csv'
 
 class Model(ABC):
@@ -19,21 +19,20 @@ class Model(ABC):
 
 class MyModel(Model):
 	def __init__(self) -> None:
-		self.data: pd.DataFrame = pd.read_csv(CSV_PATH)
-		
+	
 		try:
-			self.data_to_learn = pd.read_csv(TO_LEARN_CSV_PATH)
+			self.data = pd.read_csv(TO_LEARN_CSV_PATH)
 		except FileNotFoundError:
-			self.data_to_learn = self.data
+			self.data = pd.read_csv(COMPLETE_CSV_PATH)
 
 		self.current_data: dict = {"French": '', "English": ''}
 
 	def remove_data(self, data) -> None:
-		self.data_to_learn = self.data_to_learn[self.data_to_learn["French"] != data["French"]]
+		self.data = self.data[self.data["French"] != data["French"]]
 		self.save()
 
 	def save(self) -> None:
-		self.data_to_learn.to_csv(TO_LEARN_CSV_PATH, index=False)
+		self.data.to_csv(TO_LEARN_CSV_PATH, index=False)
 
 	def generate_data(self) -> dict:
 		self.current_data = self.data.sample(n=1, axis='rows').to_dict(orient='records')[0]
